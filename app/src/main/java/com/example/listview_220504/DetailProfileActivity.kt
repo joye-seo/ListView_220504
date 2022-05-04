42be73ae6d18df4f8eff6f1a255a389975665f91
joye <ssu1019@naver.com>
2022-05-04 14:39:33 +0900

    Initial commit

---
+++ app/src/main/java/com/example/listview_220504/DetailProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.example.listview_220504
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_profile)
+    }
+}
